ecc26be9e45ee9230f3436e1756709ac01983a06
FahimJadid <b170606023@pharm.jnu.ac.bd>
2024-11-26 07:48:21 +0600

    vercel deploy

--- apps/customer/next.config.mjs
+++ apps/customer/next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    transpilePackages: ['@repo/ui', '@repo/db'],
+  transpilePackages: ["@repo/ui", "@repo/db"],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [...config.externals, "@prisma/client"];
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
